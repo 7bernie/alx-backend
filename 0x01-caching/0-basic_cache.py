dc86fc1f7f9b64212e77d955b7f26986c24a47db
7bernie <bernardbassie@gmail.com>
2024-05-08 10:24:23 +0000

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,40 +1,24 @@
-#!/usr/bin/env python3
-""" BaseCaching module
+#!/usr/bin/python3
+""" 0-Basic dictionary
 """
-from base_caching import BaseCaching
+
+
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """ BasicCache class. Inherits from BaseCaching.
     """
-    Defines a class for caching information in key-value pairs
-    Methods:
-        put(key, item) - store a key-value pair
-        get(key) - retrieve the value associated with a key
-    """
-
-    def __init__(self):
-        """
-        Initialize the class using the parent class __init__ method
-        """
-        BaseCaching.__init__(self)
 
     def put(self, key, item):
+        """ Must assign to the dictionary self.cache_data the item value for
+        the key key. If key or item is None, this method should not do anything
         """
-        Store a key-value pair
-        Args:
-            Key
-            Item
-        """
-        if key is None or item is None:
-            pass
-        else:
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        """ Must return the value in self.cache_data linked to key. If key is
+        None or if the key doesn’t exist in self.cache_data, return None.
         """
-        Return value linked to key.
-        If key is None or doesn't exist, return None
-        """
-        if key is not None and key in self.cache_data.keys():
-            return self.cache_data[key]
-        return None
+        return self.cache_data.get(key, None)
