fdb307e56aadb327cf2b2b191efe1ae73264857f
7bernie <bernardbassie@gmail.com>
2024-05-15 12:30:07 +0000

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -64,18 +64,13 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=True)
 def index() -> str:
     """
     Handles / route
     """
-    login_as = request.args.get('login_as', type=int)
-    locale = request.args.get('locale', type=str)
-    
-    # Use the login_as and locale variables in your logic as needed
-    
     return render_template('5-index.html')
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0", debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
